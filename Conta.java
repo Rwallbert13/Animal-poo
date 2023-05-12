09cc4543398bedce403e5a60586a58ca8185caa7
Renato Walbert Bastos de Menezes <129062191+Rwallbert13@users.noreply.github.com>
2023-05-12 20:32:37 -0300

    Add files via upload

---
+++ Conta.java
@@ -0,0 +1,28 @@
+public class Conta {
+
+    public int Numero;
+    public String nomeAgencia;
+    public int numeroAgencia;
+    private String nomeBanco;
+    private int numeroBanco;
+    private String tipoConta;
+    private float saldo;
+
+    public void abrir() {
+    }
+
+    public void encerrar() {
+    }
+
+    public void consultar() {
+    }
+
+    public void saldo() {
+    }
+
+    public void creditar() {
+    }
+
+    public void debitar() {
+    }
+}
